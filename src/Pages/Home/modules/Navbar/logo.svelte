d38b0cdb5a95c097385ec44934b39ca9fe37ca10
Venkatesh <venky26494@yahoo.com>
2024-08-17 00:16:53 +0530

    navbar has been created

---
+++ src/Pages/Home/modules/Navbar/logo.svelte
@@ -0,0 +1,3 @@
+<a href="https://venky-123-debug.github.io/My_Portfolio/" target="_blank" class="relative flex h-10 w-10 cursor-pointer rounded-md border border-transparent border-blue-500 bg-sky-900 hover:scale-110 hover:border-blue-500">
+  <div class="absolute bottom-0 left-1 text-sm text-white">CV</div>
+</a>
